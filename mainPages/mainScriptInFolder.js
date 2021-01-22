f5a93eb137334e0b4ab27768ffd8cf46a39b1cf6
bjornorn <bjorn.ornevik@hotmail.com>
2021-01-23 00:44:17 +0100

    fixin

--- mainPages/mainScriptInFolder.js
+++ mainPages/mainScriptInFolder.js
@@ -1,25 +1,25 @@
 header();
 function header() {
   document.getElementById('header').innerHTML = `
-    <object id="header" type="text/html" data="mainPages/header.html"></object>
+    <object id="header" type="text/html" data="header.html"></object>
     `;
 }
 sidebar();
 function sidebar() {
   document.getElementById('sidebar').innerHTML = `
-    <object id="sidebar" type="text/html" data="mainPages/sidebar.html"></object>
+    <object id="sidebar" type="text/html" data="sidebar.html"></object>
     `;
 }
 // mainContent();
 // function mainContent() {
 //   document.getElementById('mainContent').innerHTML = `
-//     <object id="mainContent" type="text/html" data="mainPages/mainContent.html"></object>
+//     <object id="mainContent" type="text/html" data="mainContent.html"></object>
 //     `;
 // }
 footer();
 function footer() {
   document.getElementById('footer').innerHTML = `
-    <object id="footer" type="text/html" data="mainPages/footer.html"></object>
+    <object id="footer" type="text/html" data="footer.html"></object>
     `;
 }
 
