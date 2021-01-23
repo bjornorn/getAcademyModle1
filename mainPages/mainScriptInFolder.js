4a9fee2f661759e6e0e42d9ec188026b420d5538
bjornorn <bjorn.ornevik@hotmail.com>
2021-01-23 10:52:29 +0100

    fixin

--- mainPages/mainScriptInFolder.js
+++ mainPages/mainScriptInFolder.js
@@ -1,13 +1,13 @@
 header();
 function header() {
   document.getElementById('header').innerHTML = `
-    <object id="header" type="text/html" data="header.html"></object>
+    <object id="header" type="text/html" data="../header.html"></object>
     `;
 }
 sidebar();
 function sidebar() {
   document.getElementById('sidebar').innerHTML = `
-    <object id="sidebar" type="text/html" data="sidebar.html"></object>
+    <object id="sidebar" type="text/html" data="../sidebar.html"></object>
     `;
 }
 // mainContent();
@@ -19,7 +19,7 @@ function sidebar() {
 footer();
 function footer() {
   document.getElementById('footer').innerHTML = `
-    <object id="footer" type="text/html" data="footer.html"></object>
+    <object id="footer" type="text/html" data="../footer.html"></object>
     `;
 }
 
