adad32a8c4f7116697b01e04b72ea90fe803d0d5
bjornorn <bjorn.ornevik@hotmail.com>
2021-02-10 11:27:00 +0100

    yeah

--- Uke6/controller.js
+++ Uke6/controller.js
@@ -3,7 +3,6 @@ function velgBar(barNo) {
     chosenBar = undefined;
     inputValue = 0;
     outputMessage = undefined;
-
   } else {
     chosenBar = barNo;
     inputValue = numbers[chosenBar - 1];
@@ -17,32 +16,26 @@ function velgBar(barNo) {
 
 function endreStolpe() {
   if (inputValue <= 10 && inputValue >= 1) {
-     numbers[chosenBar - 1] = inputValue;
-     show();
-     return true;
+    numbers[chosenBar - 1] = inputValue;
+    show();
+    return true;
+  } else {
+    outputMessage = 'Ugyldig verdi. Velg en verdi fra 1 til 10';
+    show();
+    return false;
   }
-     else {
-       outputMessage = 'Ugyldig verdi. Velg en verdi fra 1 til 10';
-       show();       
-       return false;
-     }  
-     
-     
- 
 }
 
 function fjernStolpe() {
-
-  numbers.splice((chosenBar - 1), 1);
+  numbers.splice(chosenBar - 1, 1);
   chosenBar = undefined;
   show();
-
- 
 }
 
 function leggTilStolpe() {
   inputValue <= 10 && inputValue >= 1
-    ? (numbers.push(inputValue) && (outputMessage = undefined)) : outputMessage = 'Ugyldig verdi. Velg en verdi fra 1 til 10';
+    ? numbers.push(parseInt(inputValue)) && (outputMessage = undefined)
+    : (outputMessage = 'Ugyldig verdi. Velg en verdi fra 1 til 10');
 
   show();
 }
